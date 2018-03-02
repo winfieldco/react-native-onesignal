257564ce03717b1c9261bdd63098fecba0191088
Christopher Auyeung <chris@winfieldco.com>
2018-03-01 23:05:25 -0500

    remove createJSModules override updated for latest react native

--- android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
+++ android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
@@ -26,7 +26,6 @@ public class ReactNativeOneSignalPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
